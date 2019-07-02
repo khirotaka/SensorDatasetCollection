76e9bc9ca31a8ac7896144a271758f5a21286c54
Hirotaka Kawashima <k.hirotaka@me.com>
2019-07-03 00:53:25 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name='SensorDatasetCollection',
+    version='0.1-alpha',
+    packages={'': 'sdc'},
+    install_requires=[
+        'requests',
+        'tqdm',
+        'numpy',
+        'pandas'
+    ],
+    url='https://github.com/KawashimaHirotaka/SensorDatasetCollection',
+    license='MIT',
+    author='Kawashima Hirotaka',
+    author_email='',
+    description='Sensor Dataset collection from machine learning',
+    long_description=open('README.md').read(),
+)
